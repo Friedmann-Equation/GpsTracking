d73c941f46d0de45d7f39197d06eb5c29e133bf2
Pelangi Masita <pelangimasita18@gmail.com>
2023-11-20 00:07:28 +0700

    More update

--- GPS_send.py
+++ GPS_send.py
@@ -18,20 +18,17 @@ firebase = pyrebase.initialize_app(firebaseConfig)
 db = firebase.database()
 
 while True:
-    port = "COM4"  # Replace "x" with the correct COM port number, e.g., "COM3"
+    port = "/dev/ttyAMA0"
     ser = serial.Serial(port, baudrate=9600, timeout=0.5)
-    
     dataout = pynmea2.NMEAStreamReader()
     newdata = ser.readline()
     n_data = newdata.decode('latin-1')
-    
     if n_data[0:6] == '$GPRMC':
         newmsg = pynmea2.parse(n_data)
         lat = newmsg.latitude
         lng = newmsg.longitude
         gps = "Latitude=" + str(lat) + " and Longitude=" + str(lng)
         print(gps)
-        
         data = {"LAT": lat, "LNG": lng}
         db.update(data)
         print("Data sent")
--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 from flask import Flask, render_template
-from subprocess import Popen
-import serial
-import pynmea2
 
 app = Flask(__name__, static_url_path='/static')
-
-# Create a dictionary to store GPS data
-gps_data = {"latitude": 0.0, "longitude": 0.0}
 
 @app.route('/')
 def index():
@@ -14,19 +8,8 @@ def index():
 
 @app.route('/gps')
 def gps():
-    # Fetch GPS data using your GPS_read.py logic
-    port = "/dev/ttyAMA0"
-    ser = serial.Serial(port, baudrate=9600, timeout=0.5)
-    dataout = pynmea2.NMEAStreamReader()
-    newdata = ser.readline()
-    n_data = newdata.decode('latin-1')
-    
-    if n_data[0:6] == '$GPRMC':
-        newmsg = pynmea2.parse(n_data)
-        gps_data["latitude"] = newmsg.latitude
-        gps_data["longitude"] = newmsg.longitude
+    gps_data = {"latitude": 37.7749, "longitude": -122.4194}
 
-    gps_data = {"latitude": 123.456, "longitude": -789.012}  # Replace with actual data from Firebase
     return render_template('gps.html', gps_data=gps_data)
 
 if __name__ == '__main__':
