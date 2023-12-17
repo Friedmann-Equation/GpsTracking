09973c8a880af38a81693f46b8db00570bebc1e9
Pelangi Masita <pelangimasita18@gmail.com>
2023-12-17 08:47:31 +0700

    first commit

--- GPS_print.py
+++ GPS_print.py
@@ -1,26 +1,9 @@
-# import serial
-# import pynmea2
-
-# while True:
-#     port = "/dev/ttyAMA0"
-#     ser = serial.Serial(port, baudrate=9600, timeout=0.5)
-#     dataout = pynmea2.NMEAStreamReader()
-#     newdata = ser.readline()
-#     n_data = newdata.decode('latin-1')
-#     if n_data[0:6] == '$GPRMC':
-#         newmsg = pynmea2.parse(n_data)
-#         lat = newmsg.latitude
-#         lng = newmsg.longitude
-#         gps = "Latitude=" + str(lat) + " and Longitude=" + str(lng)
-#         print(gps)
-# this is GPS_print.py
-
 import time
 
 while True:
     # Simulated GPS data
     lat = 37.7749  # Replace with desired latitude
-    lng = -122.4194  # Replace with desired longitude
+    lng = -122.4194  
 
     gps = f"Latitude={lat} and Longitude={lng}"
     print(gps)
